e43b77d0f57ca475a2cf84da9bc346699261a24c
Ryan Petrich <rpetrich@gmail.com>
2016-07-27 22:11:14 -0400

    Automatically generate .tbd symbol files for iphone targets using iphone-simulator binaries so that private frameworks are linkable with the latest version of Xcode

--- makefiles/targets/Darwin/iphone.mk
+++ makefiles/targets/Darwin/iphone.mk
@@ -67,6 +67,7 @@ TARGET_CODESIGN_FLAGS ?= -S
 
 TARGET_PRIVATE_FRAMEWORK_PATH = $(SYSROOT)/System/Library/PrivateFrameworks
 TARGET_PRIVATE_FRAMEWORK_INCLUDE_PATH = $(ISYSROOT)/System/Library/PrivateFrameworks
+TARGET_PRIVATE_FRAMEWORK_FALLBACK_SOURCE_PATH = $(shell xcrun --sdk iphonesimulator --show-sdk-path)/System/Library/PrivateFrameworks
 
 include $(THEOS_MAKE_PATH)/targets/_common/darwin.mk
 include $(THEOS_MAKE_PATH)/targets/_common/darwin_flat_bundle.mk
